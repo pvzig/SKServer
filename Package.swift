6d0936e33e3a39d8f9582357cb0dcf72874bb186
Peter Zignego <pzignego@gmail.com>
2017-03-12 23:04:54 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,11 +8,14 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/SlackKit/SKCore", "4.0.0"),
         .Package(url: "https://github.com/SlackKit/SKWebAPI", "4.0.0"),
-        .Package(url: "https://github.com/Zewo/HTTPServer", majorVersion: 0)
+        .Package(url: "https://github.com/Zewo/HTTP.git", "0.14.3")
     ]
 )
 
+var dependency: Package.Dependency
 #if os(macOS) || os(iOS) || os(tvOS)
-let dependency: Package.Dependency = .Package(url: "https://github.com/pvzig/swifter.git", majorVersion: 3)
-package.dependencies.append(dependency)
+dependency = .Package(url: "https://github.com/httpswift/swifter.git", "1.3.2")
+#else
+dependency = .Package(url: "https://github.com/Zewo/HTTPServer", majorVersion: 0)
 #endif
+package.dependencies.append(dependency)
--- Sources/SKServer/Conformers/ZewoServer.swift
+++ Sources/SKServer/Conformers/ZewoServer.swift
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#if os(Linux)
 import Foundation
 import HTTPServer
 
@@ -48,3 +49,4 @@ class ZewoServer: SlackKitServer {
 
     }
 }
+#endif

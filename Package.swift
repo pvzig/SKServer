293ffb9eae7078e20effecfdd8a5a00367577f28
Peter Zignego <peter.zignego@smashingboxes.com>
2017-07-14 16:46:30 -0400

    Package majorVersion

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
         Target(name: "SKServer")
     ],
     dependencies: [
-        .Package(url: "https://github.com/SlackKit/SKCore", "4.0.0"),
-        .Package(url: "https://github.com/SlackKit/SKWebAPI", "4.0.0"),
+        .Package(url: "https://github.com/SlackKit/SKCore", majorVersion: 4),
+        .Package(url: "https://github.com/SlackKit/SKWebAPI", majorVersion: 4),
         .Package(url: "https://github.com/httpswift/swifter.git", "1.3.3")
     ]
 )
